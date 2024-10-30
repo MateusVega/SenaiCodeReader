86a351ae90c3e1aa60a9221bb331363eb35c1d22
mateusvega <mateusggvega@gmail.com>
2024-10-29 21:08:11 -0300

    migrate001

--- reader/urls.py
+++ reader/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.scanning, name='scanning'),
+    path('', views.scan, name='scanning'),
     path('add/', views.add, name='add'),
     path('save_qr_data/', views.save_qr_data, name='save_qr_data')
 ]
